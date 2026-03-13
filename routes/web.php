8b798630f2fe1e42e0d234ac574a4358e27ec696
viviansarahdiva25-ui <viviansarahdiva25@gmail.com>
2026-03-13 20:47:10 +0700

    add event routes

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\EventController;
 
 
 Route::get('/login', [LoginController::class, 'index']);
 Route::post('/login', [LoginController::class, 'login']);
 Route::get('/logout', [LoginController::class, 'logout']);
 Route::get('/dashboard', [DashboardController::class, 'index']);
+Route::get('/event', [EventController::class, 'index']);
