1d10d1730f35a5608dc732c6467bb9c2f67e1820
viviansarahdiva25-ui <viviansarahdiva25@gmail.com>
2026-03-13 20:46:39 +0700

    add dashboard routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\DashboardController;
+
 
 Route::get('/login', [LoginController::class, 'index']);
 Route::post('/login', [LoginController::class, 'login']);
 Route::get('/logout', [LoginController::class, 'logout']);
+Route::get('/dashboard', [DashboardController::class, 'index']);
