7fde65c247dea7b330c96e7ba035dd56b9805aae
lusianapnggbn-cell <lusipanggabean9@gmail.com>
2026-03-13 20:53:14 +0700

    add event views

---
+++ resources/views/event.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Event</title>
+</head>
+<body>
+
+<h1>Daftar Event</h1>
+
+<a href="/event/create">Tambah Event</a>
+
+<ul>
+<li>Event Seminar Teknologi</li>
+<li>Event Workshop Programming</li>
+</ul>
+
+</body>
+</html> 
