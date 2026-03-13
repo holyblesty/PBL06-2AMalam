dc21fc51e0fca930db8963c5ad9917af22dfa6f8
viviansarahdiva25-ui <viviansarahdiva25@gmail.com>
2026-03-13 20:43:03 +0700

    add logout routes

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\LoginController;
 
 Route::get('/login', [LoginController::class, 'index']);
 Route::post('/login', [LoginController::class, 'login']);
-
+Route::get('/logout', [LoginController::class, 'logout']);
