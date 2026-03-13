36cfeebe20b1eece8fb5147fc15318286b8f66bf
lusianapnggbn-cell <lusipanggabean9@gmail.com>
2026-03-13 20:39:32 +0700

    add dashboard view

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Dashboard</title>
+</head>
+<body>
+
+<h1>Dashboard Admin</h1>
+
+<ul>
+<li><a href="/event">Kelola Event</a></li>
+<li><a href="/ticket">Kelola Tiket</a></li>
+<li><a href="/register">Pendaftaran</a></li>
+</ul>
+
+</body>
+</html>
